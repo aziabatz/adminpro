9bcb0d2d5f1940de31bbc1a8dbcfb70352a52107
Ahmed Ziabat Ziabat <ahmed.ziabat@viewnext.com>
2023-04-14 13:26:22 +0200

    Fin sección 3: Diseño

---
+++ src/app/pages/notfound/notfound.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notfound',
+  templateUrl: './notfound.component.html',
+  styleUrls: [
+    './notfound.component.css'
+  ]
+})
+export class NotfoundComponent {
+
+  year: number = new Date().getFullYear();
+
+}
